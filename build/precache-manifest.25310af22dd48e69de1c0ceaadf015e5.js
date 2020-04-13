c85e69350ba92514f96c3b13ee1cecac1732d5e0
Avisrur <nadavavisrur@gmail.com>
2020-04-13 16:19:07 +0300

    finished website and deploy with gh-pages

---
+++ build/precache-manifest.25310af22dd48e69de1c0ceaadf015e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a871f027ea8438c3eee082af26d64311",
+    "url": "/reactMonsters/index.html"
+  },
+  {
+    "revision": "69da1114a4738815d674",
+    "url": "/reactMonsters/static/css/main.ca5e7e0e.chunk.css"
+  },
+  {
+    "revision": "1abee060bfa0d5897e13",
+    "url": "/reactMonsters/static/js/2.47a7167e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactMonsters/static/js/2.47a7167e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69da1114a4738815d674",
+    "url": "/reactMonsters/static/js/main.ab24defb.chunk.js"
+  },
+  {
+    "revision": "17b81f1bfa598747e2e0",
+    "url": "/reactMonsters/static/js/runtime-main.215d808f.js"
+  }
+]);
