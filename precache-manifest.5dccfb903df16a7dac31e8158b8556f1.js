a4d12a7be9e36851856c5cfbd24e4a5594ab335b
Avisrur <nadavavisrur@gmail.com>
2020-04-13 16:17:57 +0300

    Updates

---
+++ precache-manifest.5dccfb903df16a7dac31e8158b8556f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00311c51665fd17f0da0d2fc3adafe1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5302b4dd443f46d2a209",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "23d644bbc54648b76786",
+    "url": "/static/js/2.5125d72f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5125d72f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5302b4dd443f46d2a209",
+    "url": "/static/js/main.34822141.chunk.js"
+  },
+  {
+    "revision": "5708a3ad2b699ba9ebc4",
+    "url": "/static/js/runtime-main.d751cdbb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
